5dc0293e74f1806c796fe08db42fca42fc83175f
Misaina <misa.andrianary@gmail.com>
2017-07-20 16:48:00 +0300

    fichier makamo

--- DevisAutoAro/DevisAutoAro-ejb/build/generated-sources/ap-source-output/entity/AmGaranti_.java
+++ DevisAutoAro/DevisAutoAro-ejb/build/generated-sources/ap-source-output/entity/AmGaranti_.java
@@ -6,13 +6,13 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-13T14:51:17")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-18T11:27:05")
 @StaticMetamodel(AmGaranti.class)
 public class AmGaranti_ { 
 
     public static volatile SingularAttribute<AmGaranti, String> code;
-    public static volatile SingularAttribute<AmGaranti, Integer> idamgaranti;
     public static volatile SingularAttribute<AmGaranti, String> libelle;
     public static volatile ListAttribute<AmGaranti, AmTauxgaranti> amTauxgarantiList;
+    public static volatile SingularAttribute<AmGaranti, Integer> id;
 
 }
